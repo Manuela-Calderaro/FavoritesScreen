2e41ed461e8740ae3443056df672c185783fb867
Manuela Calderaro <mcalderaro12@gmail.com>
2026-01-13 18:01:02 -0300

    Add files via upload

---
+++ colors.ts
@@ -0,0 +1,9 @@
+
+export const Colors = {
+  primaryBlue: '#5B9CFF',     // Azul do Header/Filtros
+  darkBlue: '#1C3B55',        // Texto Escuro
+  neutralWhite: '#FFFFFF',    // Fundo dos Cards
+  backgroundLight: '#F5F5F5', // Fundo da Tela
+  textInverted: '#FFFFFF',    // Texto branco
+  borderDefault: '#EEEEEE',   // Linhas finas
+};
